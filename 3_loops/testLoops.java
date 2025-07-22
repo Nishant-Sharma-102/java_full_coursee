59beb54b2e49a3501305542bf139a912b24e7c5b
Nishant Sharma <kaushiknishant2002@gmail.com>
2025-07-22 04:29:43 +0000

    3_loops

--- 3_loops/testLoops.java
+++ 3_loops/testLoops.java
@@ -29,8 +29,8 @@ public class testLoops {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the number");
         n  = sc.nextInt();
-        for(int i=1; i<=10; i++){
-            System.out.println(n*i+" ");
+        // for(int i=1; i<=10; i++){
+        
         
         }
      
