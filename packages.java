aa64bbd728c4d5f2f1c352efad8646f511a9ebcf
Nishant Sharma <kaushiknishant2002@gmail.com>
2025-07-24 03:18:15 +0000

    packages.java

---
+++ packages.java
@@ -0,0 +1,14 @@
+public class packages {
+
+
+    /*
+     packages are the collection of relevant classes and interfaces
+     inbuilt packages and userdefine packages
+     io-> util->awt->event->swing-> lang->sql these are some packages
+
+     lang packages is a defaul imported packages;
+     */
+    public static void main(String[] args) {
+        
+    }
+}
